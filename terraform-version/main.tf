edb8ba52b646c83e31a413f8fe13271956c37273
Remo Mattei <rm@rm.ht>
2020-12-05 17:38:24 -0800

    adding terraform version for vrouter

---
+++ terraform-version/main.tf
@@ -0,0 +1,5 @@
+#Azure vNet
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
---
+++ terraform-version/networks.tf
@@ -0,0 +1,16 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  location            = var.location
+  address_space       = [var.address_space]
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_servers         = var.dns_servers
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                      = var.subnet_names[count.index]
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  resource_group_name       = azurerm_resource_group.rg.name
+  address_prefixes          = [var.subnet_prefixes[count.index]]
+  count                     = length(var.subnet_names)
+}
